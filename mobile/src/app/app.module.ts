b6c8dc7169721a73cae0ec25d71e1b056168bde5
andryuha49 <andryuha49@windowslive.com>
2017-03-13 22:46:31 +0200

    First commit. Added template of mobile app.

---
+++ mobile/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { HelloIonicPage } from '../pages/hello-ionic/hello-ionic';
+import { ItemDetailsPage } from '../pages/item-details/item-details';
+import { ListPage } from '../pages/list/list';
+
+import { MaterialModule } from '@angular/material';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HelloIonicPage,
+    ItemDetailsPage,
+    ListPage
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp),
+    MaterialModule
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HelloIonicPage,
+    ItemDetailsPage,
+    ListPage
+  ],
+  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+})
+export class AppModule {}
